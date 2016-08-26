7792e7fc95441163f27038bf539907bcfbbd7a8b
BenRoss92 <benjaminjackross@gmail.com>
2016-08-26 15:29:59 +0100

    tests journeylog creates new journey

--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -4,6 +4,9 @@ describe Journeylog do
 
   let(:start_station) {double :start_station }
   let(:end_station) {double :end_station }
+  let(:journey) { double :journey }
+  let(:journey_class) { double :journey_class, new: journey }
+  subject { described_class.new(journey_class) }
 
   it 'should add an entry station to the current journey' do
     subject.start(start_station)
@@ -21,12 +24,9 @@ describe Journeylog do
     expect(subject.journeys).to include(subject.current_journey)
   end
 
-  it 'should start a new journey with an entry station' do
-    expect(subject.start(station)).to eq ([station])
+  it 'creates a new journey with the entry station' do
+      subject.start(start_station)
+      expect(journey_class).to receive(:new).with(start_station)
   end
-
-
-
-
 
 end
